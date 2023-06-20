c910a764a81bf3f58dcfd61b5884e5f7f52604c2
Sab <73727767+thesabrinahyap@users.noreply.github.com>
2023-06-20 15:41:23 +0800

    Add files via upload

--- StackLinkedList.c
+++ StackLinkedList.c
@@ -1,13 +1,13 @@
-//#include<stdio.h>
-//#include<stdlib.h>
-//#include<stdbool.h>
-//#include"StackLinkedList.h"
-//
-//void initStack(Stack *S);
-//bool isEmpty(Stack S);
-//bool isFull(Stack S);
-//void push(Stack *S, Data elem);
-//void pop(Stack *S);
-//Data peek(Stack S);
-//void displayStack(Stack S);
-//void visualizeStack(Stack S);
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include"StackLinkedList.h"
+
+void initStack(Stack *S);
+bool isEmpty(Stack S);
+bool isFull(Stack S);
+void push(Stack *S, Data elem);
+void pop(Stack *S);
+Data peek(Stack S);
+void displayStack(Stack S);
+void visualizeStack(Stack S);
